1cd0b5deb2852da9de5971596ad6e0f6dcc79df1
muttakin1991 <shawonmuttakin@gmail.com>
2022-11-03 01:57:49 +0600

    done

--- index.js
+++ index.js
@@ -51,6 +51,21 @@ async function run() {
             res.send(orders);
         });
 
+
+        app.patch('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const status = req.body.status;
+            const query = { _id: ObjectId(id) };
+            const updateDoc = {
+                $set: {
+                    status: status
+                }
+            }
+            const result = await orderCollection.updateOne(query, updateDoc);
+            res.send(result);
+        })
+
+
         app.delete('/orders/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
